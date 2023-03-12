5e943b7ae61e53b3f95f49fe41a1d6eebff8450a
Joao Guilherme <jgriquena@gmail.com>
2023-03-12 15:15:05 -0300

    home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,31 @@
 import { Header } from '../../components/Header'
+import background from '../../assets/background.png'
+import './styles.css'
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className='conteudo'>
+                <img src={background} className='background' alt='background app' />
+                <div className='info'>
+                    <div>
+                        <input name='usuario' placeholder='@username' />
+                        <button>Buscas</button>
+                    </div>
+                    <div className='perfil'>
+                        <img src='https://avatars.githubusercontent.com/u/112562688?v=4' className='profile' alt='perfil' />
+                        <div>
+                            <h3>Joao Guilherme</h3>
+                            <span>@joaogriquena</span>
+                            <p>Descrição</p>
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
